e96863006d697697d9e264f0049ca38bad341ade
kunaldec022002 <kunalingale72@gmail.com>
2024-09-09 10:19:27 +0530

    create Navbar section

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import Hero from '../Hero/Hero'
+import About from '../About/About'
+import Skills from '../Skills/Skills'
+import Project from '../Projects/Project'
+import Contact from '../Contact/Contact'
+import Footer from '../../components/Footer/Footer'
 
 export default function Home() {
   return (
     <div>
+
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Skills/>
+      <Project/>
+      <Contact/>
+      <Footer/>
 
   
       
